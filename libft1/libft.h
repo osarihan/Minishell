413b7ad8c80f6281739a1a87007d3da96887ef96
Osman Ozcan <oozcan@k2m18s04.42kocaeli.com.tr>
2022-10-28 11:35:28 +0300

    |

---
+++ libft1/libft.h
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oozcan <oozcan@student.42kocaeli.com.tr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/13 11:15:19 by osarihan          #+#    #+#             */
+/*   Updated: 2022/10/22 14:16:45 by oozcan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdarg.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+
+# ifndef OPEN_MAX
+#  define OPEN_MAX 1024
+# endif
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
+
+///////////////BASIC_LIBFT/////////////////////////////////////////////////////
+void	*ft_calloc(size_t count, size_t size);
+int		ft_isalnum(int x);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int x);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+char	*ft_itoa(int n);
+int		ft_isprint(int c);
+void	*ft_memmove(void *dest, const void *src, size_t len);
+void	*ft_memset(void *w, int val, size_t n);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+char	*ft_strchr(const char *s, int x);
+char	**ft_split(char const *s, char c);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_striteri(char *s, void (*f)(unsigned int, char *));
+char	*ft_strdup(const char *s1);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlcat(char *dest, const char *src, size_t size);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlen(const char *s);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+char	*ft_strrchr(const char *s, int x);
+char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+int		ft_tolower(int w);
+int		ft_toupper(int x);
+void	ft_bzero(void *s, size_t n);
+int		ft_atoi(const char *str);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+t_list	*ft_lstnew(void	*content);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
+t_list	*ft_lstlast(t_list *lst);
+//////////////////////////////////GET_NEXT_LINE///////////////////////////////
+char	*ft_strjoin1(char *s1, char *s2);
+size_t	ft_strlen1(char *s);
+char	*ft_strchr1(char *src, int c);
+char	*ft_read_line(char *src);
+char	*ft_save(char *src);
+char	*get_line(char *src, int fd);
+char	*get_next_line(int fd);
+/////////////////////////////////FT_PRINTF////////////////////////////////////
+int		ft_printf(const char *str, ...);
+int		ft_strchecker(va_list arg, int g);
+int		print_arry(char *str);
+int		ft_uitoa(unsigned int u);
+int		ft_bsm_say(unsigned int b);
+int		ft_putchar_vol2(char c);
+int		print_hex(unsigned int n, char c);
+int		printer_ptr(unsigned long n);
+int		print_ptr(unsigned long n);
+#endif
